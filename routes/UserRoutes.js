11f23b19510a61ca23a09ac45851f0e4989f358e
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-11-17 13:27:02 +0200

    dt
    dt

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -24,4 +24,21 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:userId', async (req, res) => {
+        try {
+            const user = await User.findOne(
+                {
+                    userId: req.params.userId
+                });
+            if (!user) {
+                return res.status(404).json({error: 'User not found'});
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json({error: err.message});
+        }
+    }
+);
+
+
 module.exports = router;
