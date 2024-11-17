f04a3a460b43a56858e48de1ee78b80d3a3d2e72
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-11-17 12:19:24 +0200

    f
    f

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,10 +9,19 @@ router.post('/', async (req, res) => {
         await newUser.save();
         res.status(201).json(newUser);
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(400).json({error: err.message});
     }
 });
 
-
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+})
 
 module.exports = router;
