ccfc0ea17b683eb0457dc6e2a52b6e5c5f456410
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-11-17 13:58:42 +0200

    s
    s

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
 });
 
 
-router.get('/user/userId', async (res, req) => {
+router.get('/user/:userId', async (res, req) => {
         try {
             const notes = await Note.find({userId: req.params.userId});
             res.json(notes);
