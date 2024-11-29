8935d7690c7f12bfc3b35da8693953bf50fdb236
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-11-29 12:41:28 +0200

    r
    r

--- models/Note.js
+++ models/Note.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
-const NoteSchema = new mongoose.Schema(
-    {
-    noteId: {type: Number, required: true},
+
+function getCurrentTime() {
+    const date = new Date();
+    return date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0');
+}
+
+const NoteSchema = new mongoose.Schema({
+    noteId: { type: Number, required: true },
     content: { type: String, required: true },
-    time: { type: String, default: () => new Date().toLocaleTimeString() },
+    time: {
+        type: String,
+        default: getCurrentTime,
+    },
     isInTrash: { type: Boolean, default: false },
     isStarred: { type: Boolean, default: false },
     isPinned: { type: Boolean, default: false },
-    fontColor: { type: Number, default: 0 },
+    fontColor: { type: Number, default: -16777216 },
     fontSize: { type: Number, default: 20 },
     fontWeight: { type: Number, default: 400 },
-    userId: {type: String, required: true}
-    }
-);
+    userId: { type: String, required: true },
+});
 
 module.exports = mongoose.model('Note', NoteSchema);
