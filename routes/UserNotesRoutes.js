11759504b8f4f0c96ba492713218931c827f7d52
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-12-01 20:49:32 +0200

    s
    s

--- routes/UserNotesRoutes.js
+++ routes/UserNotesRoutes.js
@@ -109,22 +109,28 @@ router.delete('/users/:userId', async (req, res) => {
 
 //Notes
 router.post('/notes/user/:userId', async (req, res) => {
-    const userId = req.params.userId;
+    const { userId } = req.params;
     const { noteId, content, time, isInTrash, isStarred, isPinned, fontColor, fontSize, fontWeight } = req.body;
-    if (!noteId) {
+
+    // Check for required fields
+    if (noteId === undefined || typeof noteId !== 'number') {
         return res.status(400).json({ error: "noteId is required and must be a number." });
     }
     if (!content) {
         return res.status(400).json({ error: "Content is required." });
     }
+
     try {
+        // Check for an existing note with the same noteId and userId
         const existingNote = await Note.findOne({ userId, noteId });
         if (existingNote) {
             return res.status(409).json({ error: "Duplicate noteId error. This noteId already exists for this user." });
         }
+
+        // Create and save the new note
         const newNote = new Note({
-            userId,
             noteId,
+            userId,
             content,
             time: time || new Date().toISOString(),
             isInTrash,
@@ -134,13 +140,15 @@ router.post('/notes/user/:userId', async (req, res) => {
             fontSize,
             fontWeight
         });
+
         const savedNote = await newNote.save();
         res.status(201).json(savedNote);
     } catch (err) {
-        console.error("Error saving the note:", err);
+        console.error("Error processing note post request:", err);
         res.status(500).json({ error: "Server error, please try again later.", details: err });
     }
 });
+
 
 router.get('/notes/user/:userId', async (req, res) => {
     const userId = req.params.userId.trim();
