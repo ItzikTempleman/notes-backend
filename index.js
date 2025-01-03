f126164340ceec9aa9fa02d0b5624db7b647ea51
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2025-01-03 06:39:49 +0200

    e
    e

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userNoteRoutes = require('./routes/UserNotesRoutes');
-const path = require('path');
+const path = require('path'); // Import the path module
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
- const mongoUri = process.env.MONGO_URI;
+// Connect to MongoDB
+const mongoUri = process.env.MONGO_URI;
 mongoose.connect(mongoUri)
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-
+// Middleware to parse JSON
 app.use(express.json());
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use your API routes
 app.use('/api', userNoteRoutes);
 
+// Optional: Serve the HTML file for the root route or others if needed
 app.get('/', (req, res) => {
     res.send('Welcome to Itzik API');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
